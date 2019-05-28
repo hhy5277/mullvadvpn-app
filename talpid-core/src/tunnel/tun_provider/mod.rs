b55bf5a97e38e9e8fca160a6c1a9e5830d611551
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2019-05-28 20:52:46 +0000

    Implement `Default` for `TunConfig`

--- talpid-core/src/tunnel/tun_provider/mod.rs
+++ talpid-core/src/tunnel/tun_provider/mod.rs
@@ -47,6 +47,7 @@ pub trait TunProvider: Send + 'static {
 }
 
 /// Configuration for creating a tunnel device.
+#[derive(Clone, Debug, Default)]
 pub struct TunConfig {
     /// IP addresses for the tunnel interface.
     pub addresses: Vec<IpAddr>,
